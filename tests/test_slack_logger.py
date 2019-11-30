042e3c44589736aa5c05cf230d2a2270672623b7
Ryo Miyajima <sergeant.wizard@gmail.com>
2019-11-30 12:34:25 -0800

    tests just to play around

---
+++ tests/test_slack_logger.py
@@ -0,0 +1,10 @@
+import logging
+
+import slack_logger
+
+
+def test_slack_logger():
+    handler = slack_logger.SlackLogger('https://example.com')
+    _logger = logging.getLogger('test_logger')
+    _logger.addHandler(handler)
+    _logger.warning('example message')
