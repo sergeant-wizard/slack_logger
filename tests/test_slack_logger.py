c2c9d88d479657b34b4a6c4c763b10ccd51f2a1d
Ryo Miyajima <sergeant.wizard@gmail.com>
2019-12-07 16:11:35 -0800

    update slackclogger insatnce name

--- tests/test_slack_logger.py
+++ tests/test_slack_logger.py
@@ -4,7 +4,7 @@ import slack_logger
 
 
 def test_slack_logger():
-    handler = slack_logger.SlackLogger('https://example.com')
+    handler = slack_logger.SlackHandler('https://example.com')
     _logger = logging.getLogger('test_logger')
     _logger.addHandler(handler)
     _logger.warning('example message')
